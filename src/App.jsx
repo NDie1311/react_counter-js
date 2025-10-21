e1e8916a017bcb2fa7bec48a4072f74fae6fac08
Naomi <naomidie1311@gmail.com>
2025-10-21 19:29:56 +0200

    reactcounter_ND

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,32 @@ import { useState } from 'react';
 import './App.scss';
 
 export const App = () => {
-  const [count] = useState(0);
-
+  const [count, setCount] = useState(0);
   const addOne = () => {
-    // write code here
+    setCount(prev => prev + 1);
   };
 
   const add100 = () => {
-    // write code here
+    setCount(prev => prev + 100);
   };
 
-  // DON'T change the code below
   const increase = () => {
-    if (count % 5 === 0) {
-      add100();
-    }
+    setCount(prev => {
+      const shouldAdd100 = prev % 5 === 0; // Check original value
 
-    addOne();
+      return shouldAdd100 ? prev + 101 : prev + 1; // Add 100 + 1 or just 1
+    });
   };
 
   return (
     <div className="App">
       <h1 className="App__title">{`Count: ${count}`}</h1>
-
       <button type="button" className="App__add-one" onClick={addOne}>
         Add 1
       </button>
-
       <button type="button" className="App__add-100" onClick={add100}>
         Add 100
       </button>
-
       <button type="button" className="App__increase" onClick={increase}>
         Increase
       </button>
